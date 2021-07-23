49cab407f6362a5cd2702a1cc9ebb6cc06020c8b
Ameil <ameiltailor@outlook.com>
2021-07-23 13:49:13 +0100

    Added xG data, ammended user and pw to pull from env variables

--- scraper/scraper_constants.py
+++ scraper/scraper_constants.py
@@ -1,13 +1,22 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-chromdriverpath = r"YOUR CHROMEDRIVER PATH"
+load_dotenv()
+chromdriverpath = r"C:\Users\ameil\chromedriver.exe"
 start_url = f"https://members.fantasyfootballscout.co.uk/player-stats/defenders/goal-threat/"
-user_id = os.environ.get("EMAIL_SENDER")
-password = os.environ.get("EMAIL_SENDER")
+user_id = os.environ.get("FFS_USERNAME")
+password = os.environ.get("FFS_PASSWORD")
+
 seasons = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-player_tables = ["involvement", "distribution", "goal-threat", "defending", "set-pieces", "kpi-attacking", "kpi-defending"]
-team_tables=["goal-threat","defending"]
+
+
+player_tables = ["involvement", "distribution", "goal-threat", "defending", "set-pieces","kpi-attacking","kpi-defending","expected"]
+
+player_tables = ["expected"]
+
+team_tables=["goal-threat","defending","expected"]
 cwd=Path(os.getcwd())
 player_stored_path=rf"{str(cwd.absolute())}/data/player data"
 team_stored_path =rf"{str(cwd.parent.absolute())}/data/team data"
+
