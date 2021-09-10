49746693145d348630a5b5e58dbea499375fad0c
Ameil <ameiltailor@outlook.com>
2021-09-10 21:16:34 +0100

    updated main

--- database/main.py
+++ database/main.py
@@ -3,6 +3,7 @@ from scraper.player_scraper import player_scraper
 from scraper.results_scraper import results_scraper
 from database_create import database_create
 
+
 team_scraper()
 results_scraper()
 player_scraper()
